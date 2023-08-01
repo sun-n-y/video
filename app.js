4a1d7090fa6ba412a872aa62856edcfd206be12e
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-01 14:33:30 -0400

    add video preloader gif

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const slider = document.querySelector('.slider');
 const control = document.querySelector('.control');
 const video = document.querySelector('.video');
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', () => {
+  preloader.classList.add('hide-preloader');
+});
 
 control.addEventListener('click', () => {
   if (!slider.classList.contains('slide')) {
